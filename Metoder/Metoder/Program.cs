451683171f7a69a5c43100b5e35f2cd45326effc
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-09-13 13:50:38 +0200

    DenSaknadeKronan first commit

---
+++ Metoder/Metoder/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace DenSaknadeKronan
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bullar = 25;
+            int kalle = 10;
+            int pelle = 10;
+            int tjalle = 10;
+            int rödaKorset = 0;
+
+            Console.WriteLine($"Bullarna kostar {bullar}");
+            Console.WriteLine($"Kalle har {kalle}");
+            Console.WriteLine($"Pelle har {pelle}");
+            Console.WriteLine($"Tjalle har {tjalle}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Dom betalar {kalle + pelle + tjalle} kr.");
+            int kvarEfterKöp = (kalle + pelle + tjalle) - bullar;
+            kalle -= 10;
+            pelle -= 10;
+            tjalle -= 10;
+
+            Console.WriteLine($"Kalle har nu {kalle}.");
+            Console.WriteLine($"Pelle har nu {pelle}.");
+            Console.WriteLine($"Tjalle har nu {tjalle}.");
+            Console.WriteLine($"Och dom får tillbaka {kvarEfterKöp}.");
+            Console.WriteLine();
+
+            Console.WriteLine($"De delar så att de får en krona var.");
+            kalle++;
+            pelle++;
+            tjalle++;
+            kvarEfterKöp -= 3;
+            rödaKorset += kvarEfterKöp;
+            Console.WriteLine($"Kalle har nu {kalle}");
+            Console.WriteLine($"Pelle har nu {pelle}");
+            Console.WriteLine($"Tjalle har nu {tjalle}");
+            Console.WriteLine($"Och donerar {kvarEfterKöp} till Röda korset");
+            kvarEfterKöp = 0;
+            Console.WriteLine();
+
+            Console.WriteLine($"Summa summarum:");
+            int utlägg = (10 - 1) * 3;
+            Console.WriteLine($"De betalade 10 - 1 kronor var, alltså 9*3 = {utlägg} kronor");
+            Console.WriteLine($"och donerade 2 kronor");
+            utlägg += 2;
+            Console.WriteLine($"Summan blir då: {utlägg}");
+            if (utlägg != 30) Console.WriteLine($"Error 404: Krona not found");
+        }
+    }
+}

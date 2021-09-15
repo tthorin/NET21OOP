53400de5052a570e6ebde6f5f30c26096a0efd35
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-09-15 14:57:29 +0200

    Kalender done enough

---
+++ Metoder/Kalender/CalendarPage.cs
@@ -0,0 +1,184 @@
+﻿namespace Kalender
+{
+    using System;
+    //============start Class CalendarPage=============================
+    static class CalendarPage
+    {
+        static int Year;
+        static string Month;
+        static string[] MonthNames =
+            {
+                "Januari",
+                "Februari",
+                "Mars",
+                "April",
+                "Maj",
+                "Juni",
+                "Juli",
+                "Augusti",
+                "September",
+                "Oktober",
+                "November",
+                "December"
+            };
+        static string[] DayNames =
+        {
+                "Måndag",
+                "Tisdag",
+                "Onsdag",
+                "Torsdag",
+                "Fredag",
+                "Lördag",
+                "Söndag"
+            };
+        static int StartFrom;
+        static int MonthLength;
+        static int DayWidth;
+        static int TitleWidth;
+        static string Dataline = "│(0)│(1)│(2)│(3)│(4)│(5)│(6)│";
+        static string Separator;
+        static string BottomSeparator;
+        static int Pos;
+
+        static void SetUpPage(int year, int month)
+        {
+            Year = year;
+            Month = MonthNames[month - 1];
+            DateTime startDate = new DateTime(year, month, 1);
+            DayOfWeek weekDay = startDate.DayOfWeek;
+            StartFrom = (int)weekDay;
+            StartFrom--;
+            if (StartFrom < 0) StartFrom = 6;
+            MonthLength = DateTime.DaysInMonth(year, month);
+            int pageWidth = Console.WindowWidth - 1;
+            DayWidth = pageWidth / 7;
+            TitleWidth = DayWidth * 7;
+            DayWidth--;
+            TitleWidth--;
+            Separator = SetupSeparator();
+            BottomSeparator = SetupBottomSeparator();
+        }
+        static string SetupSeparator()
+        {
+            string separator = "";
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0) separator += "├" + new string('─', DayWidth);
+                else separator += "┼" + new string('─', DayWidth);
+            }
+            separator += "┤";
+            return separator;
+        }
+        static string SetupBottomSeparator()
+        {
+            string bottomCalendarSeparator = "";
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0) bottomCalendarSeparator += "└" + new string('─', DayWidth);
+                else bottomCalendarSeparator += "┴" + new string('─', DayWidth);
+            }
+            bottomCalendarSeparator += "┘";
+            return bottomCalendarSeparator;
+        }
+        static public void Print(int year, int month)
+        {
+            SetUpPage(year, month);
+            Console.Clear();
+            Pos = StartFrom;
+            PrintTitle();
+            PrintTopSeparator();
+            PrintDayBar();
+            string firstRow = PrepareFirstRow();
+            string lastRow = PrintMainPart(firstRow);
+            if (Pos > 0) PrintLastRow(lastRow);
+        }
+        static void PrintTitle()
+        {
+            string output = Month + " " + Year + " ";
+            output = output.PadLeft(TitleWidth);
+
+            Console.WriteLine("┌" + new string('─', TitleWidth) + "┐");
+            Console.WriteLine("│" + output + "│");
+            return;
+        }
+        static void PrintTopSeparator()
+        {
+            string topCalendarSeparator = "";
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0) topCalendarSeparator += "├" + new string('─', DayWidth);
+                else topCalendarSeparator += "┬" + new string('─', DayWidth);
+            }
+            topCalendarSeparator += "┤";
+            Console.WriteLine(topCalendarSeparator);
+            return;
+        }
+        static void PrintDayBar()
+        {
+            string dayLine = Dataline;
+            for (int i = 0; i < 7; i++)
+            {
+                string number = "(" + i + ")";
+                string output = " " + DayNames[i].PadRight(DayWidth);
+                output = output.Substring(0, DayWidth);
+                dayLine = dayLine.Replace(number, output);
+            }
+            Console.WriteLine(dayLine);
+            Console.WriteLine(Separator);
+        }
+        static string PrepareFirstRow()
+        {
+            string firstRow = Dataline;
+            string emptyLine = new(' ', DayWidth);
+            for (int day = 0; day < StartFrom; day++)
+            {
+                string number = $"({day})";
+                firstRow = firstRow.Replace(number, emptyLine);
+            }
+            return firstRow;
+        }
+        static string PrintMainPart(string firstRow)
+        {
+            string emptyLine = new(' ', DayWidth);
+            string dayLine = firstRow;
+            string infoLine = firstRow;
+            for (int day = 1; day <= MonthLength; day++)
+            {
+                string number = $"({Pos})";
+                string output = " " + day.ToString().PadRight(DayWidth).Substring(0, DayWidth - 1);
+                dayLine = dayLine.Replace(number, output);
+                infoLine = infoLine.Replace(number, emptyLine);
+                Pos++;
+                if (Pos > 6)
+                {
+                    Pos = 0;
+                    Console.WriteLine(dayLine);
+                    Console.WriteLine(infoLine);
+                    Console.WriteLine(infoLine);
+                    if (day == MonthLength) Console.WriteLine(BottomSeparator);
+                    else Console.WriteLine(Separator);
+                    dayLine = Dataline;
+                    infoLine = Dataline;
+                }
+            }
+            return dayLine;
+        }
+        static void PrintLastRow(string lastRow)
+        {
+            string emptyLine = new(' ', DayWidth);
+            string dayLine = lastRow;
+            string infoLine = Dataline;
+            for (int day = 0; day < 7; day++)
+            {
+                string number = $"({day})";
+                if (day >= Pos) dayLine = dayLine.Replace(number, emptyLine);
+                infoLine = infoLine.Replace(number, emptyLine);
+            }
+            Console.WriteLine(dayLine);
+            Console.WriteLine(infoLine);
+            Console.WriteLine(infoLine);
+            Console.WriteLine(BottomSeparator);
+        }
+    }
+    //============End Class CalendarPage=============================
+}
--- Metoder/Kalender/Program.cs
+++ Metoder/Kalender/Program.cs
@@ -1,157 +1,26 @@
-﻿using System;
-
-namespace Kalender
+﻿namespace Kalender
 {
+    using System;
     class Program
     {
         static void Main()
-        {
-            int month = 0;
-            int year = DateTime.Now.Year;
-            string[] monthNames =
-            {
-                "Januari",
-                "Februari",
-                "Mars",
-                "April",
-                "Maj",
-                "Juni",
-                "Juli",
-                "Augusti",
-                "September",
-                "Oktober",
-                "November",
-                "December"
-            };
-            string[] dayNames =
-            {
-                "Måndag",
-                "Tisdag",
-                "Onsdag",
-                "Torsdag",
-                "Fredag",
-                "Lördag",
-                "Söndag"
-            };
-            Console.WriteLine("Kalender");
-            
-            while (month < 1 || month > 12)
-            {
-                month = AskForNumber("Ange månad (1-12): ");
-            }
-
-            DateTime startDate = new DateTime(year, month, 1);
-            DayOfWeek weekDay = startDate.DayOfWeek;
-            int startFrom = (int)weekDay;
-            startFrom--;
-            if (startFrom < 0) startFrom = 6;
-            int monthLength = DateTime.DaysInMonth(year, month);
-
-            int pageWidth = Console.WindowWidth - 1;
-            int dayWidth = pageWidth / 7;
-            int titleWidth = dayWidth * 7;
-            dayWidth--;
-            titleWidth--;
-            //┌┐└┘─│├┤┬┴┼            
-            string output = monthNames[month - 1] + " " + year + " ";
-            output = output.PadLeft(titleWidth);
-
-            Console.WriteLine("┌" + new string('─', titleWidth) + "┐");
-            Console.WriteLine("│" + output + "│");
-
-            string topCalendarSeparator = "";
-            for (int i = 0; i < 7; i++)
-            {
-                if (i == 0) topCalendarSeparator += "├" + new string('─', dayWidth);
-                else topCalendarSeparator += "┬" + new string('─', dayWidth);
-            }
-            topCalendarSeparator += "┤";
-
-            string calendarSeparator = "";
-            for (int i = 0; i < 7; i++)
-            {
-                if (i == 0) calendarSeparator += "├" + new string('─', dayWidth);
-                else calendarSeparator += "┼" + new string('─', dayWidth);
-            }
-            calendarSeparator += "┤";
-
-            string bottomCalendarSeparator = "";
-            for (int i = 0; i < 7; i++)
-            {
-                if (i == 0) bottomCalendarSeparator += "└" + new string('─', dayWidth);
-                else bottomCalendarSeparator += "┴" + new string('─', dayWidth);
-            }
-            bottomCalendarSeparator += "┘";
-
-            Console.WriteLine(topCalendarSeparator);
-            string dataline = "│(0)│(1)│(2)│(3)│(4)│(5)│(6)│";
-            string dayLine = dataline;
-            string infoLine = dataline;
-
-            for (int i = 0; i < 7; i++)
-            {
-                string number = "(" + i + ")";
-                output = " " + dayNames[i].PadRight(dayWidth);
-                output = output.Substring(0, dayWidth);
-                dayLine = dayLine.Replace(number, output);
-            }
-            Console.WriteLine(dayLine);
-            Console.WriteLine(calendarSeparator);
-            int pos = startFrom;
-            dayLine = dataline;
-
-            string emptyLine = new string(' ', dayWidth);
-            for (int day = 0; day < startFrom; day++)
-            {
-                string number = $"({day})";
-                dayLine = dayLine.Replace(number, emptyLine);
-                infoLine = infoLine.Replace(number, emptyLine);
-            }
-
-            for (int day = 1; day <= monthLength; day++)
-            {
-                string number = $"({pos})";
-                output = " " + day.ToString().PadRight(dayWidth).Substring(0, dayWidth - 1);
-                dayLine = dayLine.Replace(number, output);
-                infoLine = infoLine.Replace(number, emptyLine);
-                pos++;
-                if (pos > 6)
-                {
-                    pos = 0;
-                    Console.WriteLine(dayLine);
-                    Console.WriteLine(infoLine);
-                    Console.WriteLine(infoLine);
-                    if (day == monthLength) Console.WriteLine(bottomCalendarSeparator);
-                    else Console.WriteLine(calendarSeparator);
-                    dayLine = dataline;
-                    infoLine = dataline;
-                }
-            }
-            if (pos > 0)
-            {
-                for (int day = pos; day < 7; day++)
-                {
-                    string number = $"({day})";
-                    dayLine = dayLine.Replace(number, emptyLine);
-                    infoLine = infoLine.Replace(number, emptyLine);
-                }
-                Console.WriteLine(dayLine);
-                Console.WriteLine(infoLine);
-                Console.WriteLine(infoLine);
-                Console.WriteLine(bottomCalendarSeparator);
-            }
-            Console.ReadKey();
-
-
-
+        {   
+            int year = 2021;
+            int month = 9;
+            Menu(year, month);
+            Console.Clear();
         }
+
+        //============start Method int AskForNumber(string question)=============================
         static int AskForNumber(string question)
         {
             Console.Write(question);
             string input = Console.ReadLine();
             return CheckConversion(input);
         }
+        //============End Method int AskForNumber(string question)=============================
 
+        //============Start Method int CheckConversion(string stringToConvert)=============================
         static int CheckConversion(string stringToConvert)
         {
             bool converted = false;
@@ -168,5 +37,60 @@ namespace Kalender
             } while (!converted);
             return number;
         }
+        //============End Method int CheckConversion(string stringToConvert)=============================
+
+        //============Start Method void Menu (int year, int month)=============================
+        static void Menu(int year, int month)
+        {
+            bool exit = false;
+            do
+            {
+                CalendarPage.Print(year, month);
+                Console.Write("Navigera år pgUp / PgDown, piltanger mellan månader eller X eller Esc. för att avsluta.");
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                switch (input.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        month--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        month--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        month++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        month++;
+                        break;
+                    case ConsoleKey.Escape:
+                        exit = true;
+                        break;
+                    case ConsoleKey.X:
+                        exit = true;
+                        break;
+                    case ConsoleKey.PageUp:
+                        year++;
+                        break;
+                    case ConsoleKey.PageDown:
+                        year--;
+                        break;
+                    default:
+                        break;
+                }
+                if (month > 12)
+                {
+                    month = 1;
+                    year++;
+                }
+                else if (month < 1)
+                {
+                    month = 12;
+                    year--;
+                }
+            } while (!exit);
+            return;
+        }
+        //============End Method void Menu (int year, int month)=============================
     }
+
 }

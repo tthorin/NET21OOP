c904888dea2cf8e11c4f091ec4b1a83019eb0324
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-09-16 14:01:20 +0200

    dictionary demo

---
+++ v37/DictionaryDemo/DictionaryDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace DictionaryDemo
+{
+    using System;
+    using System.Collections.Generic;
+    class Program
+    {
+        static void Main()
+        {
+            SortedDictionary<string, string> names = new();
+            names.Add("Batman", "Bruce Wayne");
+            names.Add("Superman", "Clark Kent");
+            names.Add("Catwoman", "Celina Kyle");
+            names.Add("Equalizer", "Denzel Washington");
+
+            foreach (KeyValuePair<string,string> kvp in names)
+            {
+                Console.WriteLine(kvp.Value + " is " + kvp.Key);
+            }
+        }
+    }
+}

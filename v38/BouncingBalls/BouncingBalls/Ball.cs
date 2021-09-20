311731954e0c6b3ba05f1dbf519caef8c130d6f7
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-09-20 13:49:21 +0200

    balls

---
+++ v38/BouncingBalls/BouncingBalls/Ball.cs
@@ -0,0 +1,57 @@
+﻿namespace BouncingBalls
+{
+    using System;
+    internal class Ball
+    {
+        int X = 0;
+        int Y = 0;
+        int PlusX = 1;
+        int PlusY = 1;
+        string BallShape = "O";
+
+        public Ball(int plusX, int plusY, string ballShape)
+        {
+            PlusX = plusX;
+            PlusY = plusY;
+            BallShape = ballShape;
+        }
+        public Ball()
+        {
+            PlusX = 1;
+            PlusY = 1;
+        }
+        public Ball(string ballShape)
+        {
+            PlusX = 1;
+            PlusY = 1;
+            BallShape = ballShape;
+        }
+
+        private void CheckBoundaries()
+        {
+            if (X <= 1 || X >= Console.WindowWidth -1)
+            {
+                PlusX = -PlusX;
+            }
+            if (Y <= 1 || Y >= Console.WindowHeight - 1)
+            {
+                PlusY = -PlusY;
+            }
+        }
+        public void MoveIt()
+        {
+            CheckBoundaries();
+            Console.SetCursorPosition(X, Y);
+            Console.Write(" ");
+            X += PlusX;
+            Y += PlusY;
+            Console.SetCursorPosition(X, Y);
+            Console.Write(BallShape);
+        }
+        public void SetBallPosition(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ v38/BouncingBalls/BouncingBalls/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BouncingBalls
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball ballOne = new Ball();
+            Ball ballTwo = new Ball(1, -1, "*");
+            Ball ballThree = new Ball(-1, 1, "#");
+            ballOne.SetBallPosition(10, 10);
+            ballTwo.SetBallPosition(20, 5);
+            ballThree.SetBallPosition(60, 20);
+            Console.CursorVisible = false;
+            while (true)
+            {
+                ballOne.MoveIt();
+                ballTwo.MoveIt();
+                ballThree.MoveIt();
+                Thread.Sleep(75);
+            }
+        }
+    }
+}

afc04eafae418e9b85dd1e4ced0f5368f7ab7e82
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-09-24 16:10:22 +0200

    first commit

---
+++ v38/ClassObjects/ClassObjects/MovieInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassObjects
+{
+    using System;
+    using System.Collections.Generic;
+    internal class MovieInformation
+    {
+        public string Name { get; set; } = "";
+        public string Actors { get; set; } = "";
+        public int StarRating { get; set; } = 0;
+        
+        public void PrintMovie()
+        {
+            Console.WriteLine(Name + " - " + Actors);
+            Console.WriteLine("Stars: " + new string('*', StarRating) + "\n");
+        }
+    }
+}
---
+++ v38/ClassObjects/ClassObjects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<MovieInformation> movieList = new List<MovieInformation>()
+            {
+                    new MovieInformation()
+                    {
+                        Name = "Elf",
+                        Actors = "Will Ferrel",
+                        StarRating = 1
+                    },
+                    new MovieInformation()
+                    {
+                        Name = "Death Wish",
+                        Actors = "Bruce Willis",
+                        StarRating = 4
+                    },
+                    new MovieInformation()
+                    {
+                        Name = "Malignant",
+                        Actors = "Annabelle Wallis, Maddie Hasson",
+                        StarRating = 10
+                    },
+                    new MovieInformation()
+                    {
+                        Name = "Death Note",
+                        Actors = "Nat Wolff, LaKeith Stanfield, Margaret Qualley",
+                        StarRating = 3
+                    }
+            };
+            foreach (var movie in movieList)
+            {
+                movie.PrintMovie();
+            }
+
+
+        }
+    }
+}

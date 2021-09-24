dc5385091d853cb41838abe410daed3f397eb93e
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-09-24 13:42:13 +0200

    Cars first commit

---
+++ v38/Cars/Cars/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace Cars
+{
+    using System;
+
+    class Car
+    {
+        public int Wheels { get; set; } = 4;
+        public int SteeringWheel { get; set; } = 1;
+        public int Doors { get; set; } = 4;
+        public int Gears { get; set; } = 6;
+        public string Model { get; set; } = "Standard";
+        public string Manufacurer { get; set; } = "CarCorp";
+        public int Year { get; set; } = 2021;
+
+        public void Start()
+        {
+            Console.WriteLine("You start the car.");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("You stop the car.");
+        }
+        public void GearUp()
+        {
+            Console.WriteLine("You change to a higher gear.");
+        }
+        public void GearDown()
+        {
+            Console.WriteLine("You change to a lower gear.");
+        }
+        public void GoFaster()
+        {
+            Console.WriteLine("You speed up.");
+        }
+        public void GoSlower()
+        {
+            Console.WriteLine("You slow down.");
+        }
+    }
+}

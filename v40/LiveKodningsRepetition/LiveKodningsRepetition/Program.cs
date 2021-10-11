9b0ffe948a763878385dd2c42e1faabd8cc244d1
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2021-10-11 20:54:40 +0200

    v40

---
+++ v40/LiveKodningsRepetition/LiveKodningsRepetition/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for (int i =11 ; i < 11; i++)
+{
+    Console.Write(i+" ");
+    int[] intArr = new int[]{ 1,2,3,4,5,6};
+    intArr.GetUpperBound(0);
+    var name = "Magika";
+
+    for (int i = name.ToArray().GetUpperBound(0); i <= 0; i++)
+    {
+
+    }
+}
---
+++ v40/LiveKodningsRepetition/LiveKodningsRepetition/Test.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Test
+{
+    public class Test
+    {
+        public string Name { get; set; } = "";
+        
+        
+        
+        public static void Gah()
+        {
+            int x = 5;
+            int y = 5 + x;
+            List<string> myList = new();
+            myList.Add("Pelle");
+            myList.ToArray().GetUpperBound(0);
+
+        }
+
+
+    }
+}
